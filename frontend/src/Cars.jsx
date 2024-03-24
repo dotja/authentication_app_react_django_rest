6a9238ebf4ebbb6353d18b58883dfb2440afb21c
breshlyabanid22 <breshlyabanid@gmail.com>
2024-03-24 16:00:22 +0800

    car sorting function done

--- frontend/src/Cars.jsx
+++ frontend/src/Cars.jsx
@@ -8,7 +8,8 @@ import {
 
 const Cars = () => {
   const [carData, setCarData] = useState([]);
-
+  const [search, setSearch] = useState("");
+  const [order, setOrder] = useState("ASC");
   const baseUrl = "http://localhost:8000";
 
   useEffect(() => {
@@ -25,11 +26,40 @@ const Cars = () => {
       .catch((err) => console.error(err));
   };
 
+  const handleSort = (filter) => {
+    if(order === 'ASC'){
+      const sorted = [...carData].sort((a, b) => {
+        if(typeof a[filter] === 'string' && typeof b[filter] === 'string'){
+          return a[filter].toLowerCase() > b[filter].toLowerCase() ? 1 : -1;
+        }else{
+          return a[filter] - b[filter];
+        }
+    });
+      setCarData(sorted);
+      setOrder('DSC');
+    }
+    if(order === 'DSC'){
+      const sorted = [...carData].sort((a, b) => {
+        if(typeof a[filter] === 'string' && typeof b[filter] === 'string'){
+          return a[filter].toLowerCase() < b[filter].toLowerCase() ? 1 : -1;
+        }else{
+          return b[filter] - a[filter];
+        }
+    });
+      setCarData(sorted);
+      setOrder('ASC');
+    }
+  }
+
   return (
     <>
       <div className="relative h-full bg-black-2 flex flex-col items-center pt-20 pb-20 mx-auto text-white lg:w-10/12">
         <span className="relative h-[40px] w-[600px] mt-4">
-          <input className="h-[40px] w-[600px] border border-yellow py-1 px-4 text-sm rounded-full bg-inherit" />
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Find your desired luxury car"
+            className="h-[40px] w-[600px] border border-yellow py-1 px-4 text-xs rounded-full bg-inherit"
+          />
           <FontAwesomeIcon
             icon={faMagnifyingGlass}
             className="absolute right-4 top-3 text-yellow"
@@ -42,19 +72,27 @@ const Cars = () => {
               Explore a variety of our luxury cars available for rent.
             </p>
             <div className="flex gap-8 2xl:gap-10">
-              <button className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
+              <button 
+              onClick={() => handleSort('daily_rate')}
+              className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
                 Price
                 <FontAwesomeIcon icon={faAngleDown} />
               </button>
-              <button className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
+              <button 
+              onClick={() => handleSort('model_year')}
+              className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
                 Year
                 <FontAwesomeIcon icon={faAngleDown} />
               </button>
-              <button className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
+              <button 
+              onClick={() => handleSort('make')}
+              className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
                 Brand
                 <FontAwesomeIcon icon={faAngleDown} />
               </button>
-              <button className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
+              <button 
+              // onClick={() => handleSort('daily_rate')}
+              className="flex items-center gap-2 border px-4 py-1 rounded-lg hover:text-yellow hover:border-yellow">
                 Rating
                 <FontAwesomeIcon icon={faAngleDown} />
               </button>
@@ -63,8 +101,13 @@ const Cars = () => {
         </div>
         <div className="flex flex-row flex-wrap justify-center items-start gap-4 w-full mt-4 lg:mb-8 2xl:mb-14 p-1">
           {carData &&
-            carData.map((car, index) => {
-              return (
+            carData
+              .filter((car) => {
+                return search.toLowerCase() === ""
+                  ? car
+                  : car.make.toLowerCase().includes(search.toLowerCase());
+              })
+              .map((car, index) => (
                 <div
                   key={index}
                   className="relative w-[250px] h-[270px] 2xl:w-[370px] 2xl:h-[390px] hover:scale-105 duration-75"
@@ -78,8 +121,7 @@ const Cars = () => {
                     <p>$ {car.daily_rate}/day</p>
                   </div>
                 </div>
-              );
-            })}
+              ))}
         </div>
       </div>
     </>
